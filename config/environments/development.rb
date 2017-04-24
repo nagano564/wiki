ff8123841dbfa56b06a0bf63c37a99f3a55ec83f
nagano564 <nagano564@gmail.com>
2017-04-24 12:19:04 -0400

    add mailtrap and change confirmable. Doesn't work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,14 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => 'd5d0f8591938eb',
+  :password => '3cf1d39d8d0de8',
+  :address => 'smtp.mailtrap.io',
+  :domain => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+  }
 end
